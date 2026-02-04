a4b767dc471b4878f69eab5b0433f5edb7658dfe
eliotc <me@eliotc.com>
2026-02-04 11:24:44 -0800

    Initial Commit

---
+++ life-encyclopedia/life_encyclopediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  life_encyclopediaApp.swift
+//  life-encyclopedia
+//
+//  Created by Eliot Chang on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct life_encyclopediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
